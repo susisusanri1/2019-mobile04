3d85cc6444c60c21e8c7cab5cef5ee34190dbd70
susi susanti <susisusanti4266@gmail.com@susisusanti4266@gmail.com>
2019-10-02 20:38:44 +0700

    Menambahkan hasil profil dari bundle

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -3,19 +3,29 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_bundle);
 
-        // TODO: bind here
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            // TODO: display value here
+            usernameText.setText(extras.getString(BundleActivity.USERNAME_KEY));
+            nameText.setText(extras.getString(BundleActivity.NAME_KEY));
+            ageText.setText(String.valueOf(extras.getInt(BundleActivity.AGE_KEY)));
         }
+
+
     }
 }

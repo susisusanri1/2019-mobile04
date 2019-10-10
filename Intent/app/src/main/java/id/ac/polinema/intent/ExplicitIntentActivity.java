065d08781037e6a8db222f36ddf1d51d6437be4c
susi susanti <susisusanti4266@gmail.com@susisusanti4266@gmail.com>
2019-10-10 22:53:32 +0700

    init Commit Explicit Intent

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -8,11 +8,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
+    private EditText nameInput;
+    private TextView nameOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        nameInput = findViewById(R.id.input_name);
+        nameOutput =findViewById(R.id.text_output);
     }
-
+    public void handleSubmit(View view){
+        String name = nameInput.getText().toString();
+        nameOutput.setText("Hello " +name+", Congratulation");
+    }
 }
